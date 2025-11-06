191a5f2998e0ed84fd147c732b33e9e8a8e57b87
hadi <haditj66@hotmail.com>
2025-11-06 09:35:03 +0900

    MODULE_DEPENDS_NONQRwas added to cmake rosqt

--- rosqt/IF/ourcolcon.bash
+++ rosqt/IF/ourcolcon.bash
@@ -1,2 +1,20 @@
 #!/bin/bash
-colcon build --build-base build/lincol --event-handlers console_cohesion+ --install-base install_lin
+
+is_raspberry_pi() {
+  if [ -f /proc/device-tree/model ]; then
+    if grep -q "Raspberry Pi" /proc/device-tree/model; then
+      return 0
+    fi
+  fi
+  return 1
+}
+
+
+if is_raspberry_pi; then
+  echo "This is a Raspberry Pi. Running Raspberry Pi-specific actions..."
+  colcon build --build-base build/linrpicol --event-handlers console_cohesion+ --install-base install_linrpi
+else
+  echo "This is not a Raspberry Pi. Running alternative actions..."
+  colcon build --build-base build/lincol --event-handlers console_cohesion+ --install-base install_lin
+fi
+
--- rosqt/oursource.bash
+++ rosqt/oursource.bash
@@ -1,2 +1,20 @@
 #!/bin/bash
-source ./install_lin/setup.bash
+
+is_raspberry_pi() {
+  if [ -f /proc/device-tree/model ]; then
+    if grep -q "Raspberry Pi" /proc/device-tree/model; then
+      return 0
+    fi
+  fi
+  return 1
+}
+
+if is_raspberry_pi; then
+  echo "This is a Raspberry Pi. Running Raspberry Pi-specific actions..."
+source ./install_linrpi/setup.bash
+else
+  echo "This is not a Raspberry Pi. Running alternative actions..."
+  source ./install_lin/setup.bash
+fi
+
+
